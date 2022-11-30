2ff4f4505f32116548334f5724117e17c421034e
juecheng.huang <juecheng.huang@kaiostech.com>
2022-11-30 14:43:44 +0800

    add handwashing source

--- src/Howto.svelte
+++ src/Howto.svelte
@@ -1,9 +1,13 @@
 <div bp="grid">
   <img
     bp="offset-5@md 4@md 12@sm"
-    src="washing-hands.webp"
+    src="https://happilyhealth.com/wp-content/uploads/2020/12/POST_10_MARCH-30-1024x1024.png"
     alt="how to washing your hands."
   />
+
+<a bp="offset-5@md 4@md 12@sm" 
+  href="https://happilyhealth.com/wp-content/uploads/2020/12/POST_10_MARCH-30-1024x1024.png">PNG</a>
+
 </div>
 
 <style>
